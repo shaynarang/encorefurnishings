d2dd758766ba99a7dfe94bdbcee72c87a6c31411
shaynarang <shaynarang@gmail.com>
2013-11-14 16:50:28 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Encorefurnishings::Application.config.secret_key_base = '675d0b7c3b0cba3391c891df6619cd003719621329e6e954d52bc5cab2ac30021bc303333f445cab81868fe57fbfee428580f49c9a5eb2620367eed2d7b18904'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Encorefurnishings::Application.config.session_store :cookie_store, key: '_encorefurnishings_session'
