e496f75d035fa863ab4591643b0935f8f12ed32f
shaynarang <shaynarang@gmail.com>
2013-11-15 19:06:03 -0600

    updated home page links and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Encorefurnishings::Application.routes.draw do
-  get "home/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'home#index'
 
+
+  resources :about, :only => [:index]
+  resources :services
+  resources :products
+  resources :contact, :only => [:index]
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
