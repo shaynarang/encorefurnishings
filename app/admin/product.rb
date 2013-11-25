9ee6009b3717fb80ef6e0f7b02483aef54c6af20
shaynarang <shaynarang@gmail.com>
2013-11-25 15:50:48 -0600

    added active admin product resource

---
+++ app/admin/product.rb
@@ -0,0 +1,24 @@
+ActiveAdmin.register Product do
+  index do
+    column :id
+    column :title
+    column :description
+    column "Added", :created_at
+    column "Last Updated", :updated_at
+    default_actions
+  end
+  
+  # See permitted parameters documentation:
+  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #  permitted = [:permitted, :attributes]
+  #  permitted << :other if resource.something?
+  #  permitted
+  # end
+  
+end
