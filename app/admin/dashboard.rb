98e6d7e7b4ef4d97fcc7491bf81f59ff4466cd61
shaynarang <shaynarang@gmail.com>
2013-11-25 16:57:25 -0600

    customized active admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,26 @@ ActiveAdmin.register_page "Dashboard" do
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
-    div :class => "blank_slate_container", :id => "dashboard_default_message" do
-      span :class => "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    
+    columns do
+      column do
+        panel "Recent Products" do
+          table_for Product.order("updated_at").limit(5) do
+            column :title do |product|
+              link_to product.title, [:admin, product]
+            end
+            column :description
+            column :updated_at
+          end
+        end
       end
-    end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+      column do
+        panel "Info" do
+          para "This is the Admin area for the Encore Furnishings website. You can view, add, edit, and delete products from this area. Click on the 'Products' link at the top of the page to do so. Enjoy."
+        end
+      end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
+    end #columns
   end # content
 end
